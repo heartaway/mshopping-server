5a6618de1ce2709bf6100ad04edb95dd5c31d654
mulou <mulou.zzy@taobao.com>
2014-08-28 20:32:50 +0800

    添加图片上传接口

--- src/test/java/com/taobao/mshopping/demo/persistence/BaseTestCase.java
+++ src/test/java/com/taobao/mshopping/demo/persistence/BaseTestCase.java
@@ -3,6 +3,9 @@ package com.taobao.mshopping.demo.persistence;
 import com.taobao.itest.ITestSpringContextBaseCase;
 import com.taobao.itest.annotation.ITestSpringContext;
 import com.taobao.mshopping.demo.persistence.dao.ItemHouseDao;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 
@@ -14,5 +17,9 @@ public class BaseTestCase extends ITestSpringContextBaseCase{
 
     @Resource
     ItemHouseDao itemHouseDao;
+
+    final static Logger logger = LoggerFactory.getLogger(BaseTestCase.class);
+
+
 
 }
---
+++ src/test/java/com/taobao/mshopping/demo/persistence/Tester.java
@@ -0,0 +1,20 @@
+package com.taobao.mshopping.demo.persistence;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * mulou.zzy
+ * taedemo
+ */
+public class Tester {
+    
+    final static Logger logger = LoggerFactory.getLogger(Tester.class);
+    
+    @Test
+    public void test(){
+
+            logger.error("asdasd{}", "123");
+    }
+}
